77259141e6f3e763ad53bb03024ef1a6dafc1fa7
Tharin Wilk <Tharin.m.wilk@gmail.com>
2023-07-14 18:29:18 -0400

    0.0.1 Added reminder selection, toggle, and storage

--- renderer/js/settings.js
+++ renderer/js/settings.js
@@ -1,6 +1,7 @@
 export default document.addEventListener("DOMContentLoaded", () => {
   let settings = {
-    timeout: 5000,
+    timeout: 60000,
+    reminderIndex: "RANDOM",
   };
 
   window.addEventListener("load", () => {
@@ -9,13 +10,16 @@ export default document.addEventListener("DOMContentLoaded", () => {
     if (storedSettings) {
       settings = JSON.parse(storedSettings);
       window.electronAPI.updateSettings(settings);
+    } else {
+      const jsonSettings = JSON.stringify(settings);
+      localStorage.setItem("settings", jsonSettings);
     }
 
     initDOMSettings();
   });
 
   const initDOMSettings = () => {
-    const time = settings.timeout / 100;
+    const time = settings.timeout / 60000;
     timeSlider.value = time;
     timeLabel.textContent = `Time (${time} minutes)`;
   };
@@ -28,7 +32,47 @@ export default document.addEventListener("DOMContentLoaded", () => {
     const time = timeSlider.value;
     timeLabel.textContent = `Time (${time} minutes)`;
 
-    settings.timeout = time * 1000;
+    settings.timeout = time * 60000;
+  });
+
+  // Reminder Settings
+  const reminderToggle = document.getElementById("reminder-toggle");
+  const reminderFieldset = document.getElementById("reminder-fieldset");
+  const reminderSelect = document.getElementById("reminder-select");
+
+  const createSelectOptions = () => {
+    // reset node
+    Array.from(reminderSelect.children).forEach((child) => {
+      reminderSelect.removeChild(child);
+    });
+
+    const jsonList = localStorage.getItem("list");
+    const list = JSON.parse(jsonList);
+
+    list.forEach((item) => {
+      const truncatedItem = item.length > 48 ? item.slice(0, 48) + "..." : item;
+      const option = document.createElement("option");
+      option.innerText = truncatedItem;
+      option.value = item;
+      reminderSelect.appendChild(option);
+    });
+  };
+
+  reminderToggle.addEventListener("change", (event) => {
+    const isChecked = event.target.checked;
+
+    if (isChecked) {
+      createSelectOptions();
+      reminderFieldset.classList.remove("hidden");
+      settings.reminderIndex = 0;
+    } else {
+      reminderFieldset.classList.add("hidden");
+      settings.reminderIndex = "RANDOM";
+    }
+  });
+
+  reminderSelect.addEventListener("change", () => {
+    settings.reminderIndex = reminderSelect.selectedIndex;
   });
 
   // Save Settings Button
