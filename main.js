b203358d791c724d2f049c85e0c72fc5a26eacdd
Tharin Wilk <Tharin.m.wilk@gmail.com>
2023-07-24 18:59:56 -0400

    0.0.2 Updated main js to work with new system

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ const isMac = process.platform === "darwin";
 function createMainWindow() {
   mainWindow = new BrowserWindow({
     title: "refocus",
-    width: isDev ? 1200 : 500,
+    width: isDev ? 1000 : 500,
     height: 600,
     show: false,
     webPreferences: {
@@ -36,9 +36,6 @@ function createMainWindow() {
 }
 
 const notificationTitle = "Reminder";
-let notificationMessage = "Remember to set a reminder.";
-let notificationSettings = { timeout: 10000 };
-let notificationInterval;
 
 const toastXml = (message) => {
   return `
@@ -52,65 +49,28 @@ const toastXml = (message) => {
   </toast>`;
 };
 
-let list = null;
-const showNotification = () => {
-  let notificationMessage;
-
-  if (notificationSettings.reminderIndex === "RANDOM") {
-    // Generate a random index within the range of the list array
-    const randomIndex = Math.floor(Math.random() * list.length);
-    notificationMessage = list[randomIndex];
-  } else {
-    // Get the reminder index from the settings object
-    const reminderIndex = notificationSettings.reminderIndex;
-    notificationMessage = list[reminderIndex];
-  }
-
-  return new Notification({
+const showNotification = (notificationMessage) => {
+  const notification = new Notification({
     toastXml: toastXml(notificationMessage),
   });
+
+  notification.show();
 };
 
 app.whenReady().then(() => {
-  ipcMain.on("set-list", (event, data) => {
-    if (data.length > 0) {
-      list = data;
-    } else {
-      notificationMessage = "Remember to set a reminder.";
-    }
-  });
-
-  ipcMain.on("update-settings", (event, data) => {
-    // Clear the existing interval if change to timeout setting
-    if (notificationSettings.timeout !== data.timeout) {
-      clearInterval(notificationInterval);
-    }
-
-    // Update notification settings
-    notificationSettings = data;
-
-    // Create a new interval with the updated timeout
-    notificationInterval = setInterval(() => {
-      let notification = showNotification();
-      notification.show();
-    }, notificationSettings.timeout);
+  ipcMain.on("show-notification", (event, data) => {
+    showNotification(data);
   });
 
   createMainWindow();
 
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createMainWindow();
+      createMainWindow(notificationMessage);
     }
   });
 
   mainWindow.show();
-
-  // Create a notification
-  notificationInterval = setInterval(() => {
-    let notification = showNotification();
-    notification.show();
-  }, notificationSettings.timeout);
 
   // Create a tray icon
   const trayIconPath = path.join(__dirname, "assets/test.jpg");
